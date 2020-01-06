3c8832c4705148f8bb7275de75cbf8d9d1cb725a
Akash Kumar <Akash@cloudthing.com>
2020-01-06 15:10:21 +0530

    Updates

---
+++ precache-manifest.7648eb5a2dbe0605517a9ed1cdacace4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef641efe7e938efde1a46071887e5308",
+    "url": "/Akash-Kumar-cloud/react-routing/index.html"
+  },
+  {
+    "revision": "5e45adf7209098108ff5",
+    "url": "/Akash-Kumar-cloud/react-routing/static/css/main.7db4b589.chunk.css"
+  },
+  {
+    "revision": "048dbace59a5f0cf16d5",
+    "url": "/Akash-Kumar-cloud/react-routing/static/js/2.ea01203e.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/Akash-Kumar-cloud/react-routing/static/js/2.ea01203e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e45adf7209098108ff5",
+    "url": "/Akash-Kumar-cloud/react-routing/static/js/main.ca45f0ef.chunk.js"
+  },
+  {
+    "revision": "503b6ee268db59e34f50",
+    "url": "/Akash-Kumar-cloud/react-routing/static/js/runtime-main.d3cf6172.js"
+  }
+]);
